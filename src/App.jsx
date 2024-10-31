e4183643a23e1e5588234e3f271460886b6d4631
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-10-31 15:52:46 -0500

    project structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold text-blue-600">
+          ¡Hola Tailwind + React!
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
