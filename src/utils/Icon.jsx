bbf30b9e6f5441b68a11e05a9e13f0e88dcec492
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-02 22:33:43 -0500

    feat (Utils): Create icon controller

---
+++ src/utils/Icon.jsx
@@ -0,0 +1,9 @@
+import LogoMr from "../assets/img/LogoMr.svg";
+import Menu from "../assets/img/MenuIcon.svg";
+import CloseMenu from "../assets/img/CloseMenuIcon.svg";
+
+export const IconsSvg = {
+  LogoMr,
+  Menu,
+  CloseMenu,
+};
