d7ff5663e665e9dd97cd237345923e103e072306
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-03 23:25:24 -0500

    refactor svg and img

--- src/utils/Icon.jsx
+++ src/utils/Icon.jsx
@@ -1,8 +1,8 @@
-import LogoMr from "../assets/img/LogoMr.svg";
-import Menu from "../assets/img/MenuIcon.svg";
-import CloseMenu from "../assets/img/CloseMenuIcon.svg";
-import Arrow from "../assets/img/ArrowIcon.svg";
-import Gradient from "../assets/img/Gradient.svg";
+import LogoMr from "../assets/svg/LogoMr.svg";
+import Menu from "../assets/svg/MenuIcon.svg";
+import CloseMenu from "../assets/svg/CloseMenuIcon.svg";
+import Arrow from "../assets/svg/ArrowIcon.svg";
+import Gradient from "../assets/svg/Gradient.svg";
 
 export const IconsSvg = {
   LogoMr,
--- src/utils/Img.jsx
+++ src/utils/Img.jsx
@@ -1,5 +1,7 @@
 import Galletas from "../assets/img/Galletas.png";
+import GomaHigado from "../assets/img/GomaHigado.png";
 
 export const Images = {
   Galletas,
+  GomaHigado,
 };
