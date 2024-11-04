2e56b3896fe4ace7f209d29c4b4053287135cb02
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-04 08:43:41 -0500

    feat: benefits icons a aux

---
+++ src/data/Benefits.jsx
@@ -0,0 +1,26 @@
+import { IconsSvg } from "../utils/Icon";
+
+export const benefits = [
+  {
+    icon: IconsSvg.Brain,
+    text: "Actividad Cerebral",
+    complement:
+      "Estimulación mental, reduciendo el aburrimiento y el comportamiento destructivo.",
+  },
+  {
+    icon: IconsSvg.Tooth,
+    text: "Limpieza dental",
+    complement:
+      "El material duro y suave de los deshidratados ayuda a raspar la placa y el sarro.",
+  },
+  {
+    icon: IconsSvg.Leaf,
+    text: "Naturales",
+    complement: "No contienen conservante",
+  },
+  {
+    icon: IconsSvg.Dog,
+    text: "Premios de alto valor",
+    complement: "Ideales para entrenar y recompensar a tu mascota.",
+  },
+];
--- src/utils/Icon.jsx
+++ src/utils/Icon.jsx
@@ -2,6 +2,10 @@ import LogoMr from "../assets/svg/LogoMr.svg";
 import Menu from "../assets/svg/MenuIcon.svg";
 import CloseMenu from "../assets/svg/CloseMenuIcon.svg";
 import Arrow from "../assets/svg/ArrowIcon.svg";
+import Dog from "../assets/svg/DogIcon.svg";
+import Leaf from "../assets/svg/LeafIcon.svg";
+import Tooth from "../assets/svg/ToothIcon.svg";
+import Brain from "../assets/svg/BrainIcon.svg";
 import Gradient from "../assets/svg/Gradient.svg";
 
 export const IconsSvg = {
@@ -9,5 +13,9 @@ export const IconsSvg = {
   Menu,
   CloseMenu,
   Arrow,
+  Dog,
+  Leaf,
+  Tooth,
+  Brain,
   Gradient,
 };
