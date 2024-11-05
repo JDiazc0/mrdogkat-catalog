e21d2724c831ad1d477c6840762aa550a20632e1
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-05 15:40:07 -0500

    feat (filter) filter products

---
+++ src/data/Filters.jsx
@@ -0,0 +1,8 @@
+export const Filters = [
+  { name: "Todo", to: "/productos/todo" },
+  { name: "Deshidratados", to: "/productos/deshidratados" },
+  { name: "Galletas", to: "/productos/galletas" },
+  { name: "Helados", to: "/productos/helados" },
+  { name: "Gomas", to: "/productos/gomas" },
+  { name: "Cumpleaños", to: "/productos/cumpleaños" },
+];
