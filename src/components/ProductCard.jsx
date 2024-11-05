da5e69750efe01214ff5f0c05a91611577b44df9
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-05 15:39:48 -0500

    feat (card) change props

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,30 +1,37 @@
+import { Link } from "react-router-dom";
+
 function ProductCard(props) {
-  const { showImage, productName, description, extraClass } = props;
+  const { product, extraClass } = props;
 
   return (
     <>
       <div
         className={`bg-white shadow-lg shadow-secondary-500/75
-            min-h-[700px] w-[600px] md:min-h-[500px] md:w-[400px]
+            min-h-[500px] w-[400px] md:min-h-[400px] md:w-[300px]
             p-4 my-5 rounded-lg
             ${extraClass}`}>
         <div className="relative justify-center items-center">
           <img
-            src={showImage}
-            alt={productName}
+            src={product.showImage}
+            alt={product.productName}
             className="w-full h-auto block"
           />
         </div>
         <div className="flex flex-col h-auto justify-end items-start">
           <h3 className="font-bold pr-4 text-3xl md:text-xl content-center">
-            {productName}
+            {product.productName}
           </h3>
-          <p className="my-1 text-lg md:text-sm">{description}</p>
-          <button
-            className="p-1 my-1 bg-primary-500 text-white 
+          <p className="my-1 text-lg md:text-sm">{product.description}</p>
+          <Link
+            to={`/productos/${product.type}/${
+              product.id
+            }/${product.productName.replace(/ /g, "-")}`}>
+            <button
+              className="p-1 my-1 bg-primary-500 text-white 
           font-medium rounded active:scale-95">
-            Ver más
-          </button>
+              Ver más
+            </button>
+          </Link>
         </div>
       </div>
     </>
