838d949a3a87e9198d86cd5b13923e2fe5766959
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-06 22:25:48 -0500

    feat(routes) add product detail route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import NavBar from "./layouts/NavBar";
 import Index from "./pages/Index";
 import Products from "./pages/Products";
 import AboutUs from "./pages/AboutUs";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
@@ -12,6 +13,10 @@ function App() {
         <Route path="/" element={<Navigate to="/inicio" replace />} />
         <Route path="/inicio" element={<Index />} />
         <Route path="/productos/:type" element={<Products />} />
+        <Route
+          path="/productos/:type/:id/:productName"
+          element={<ProductDetail />}
+        />
         <Route path="/nosotros" element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
