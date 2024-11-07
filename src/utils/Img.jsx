04824ffccfad0206d3163584fa430dc96682dada
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-07 11:12:03 -0500

    fix deploy

--- src/utils/Img.jsx
+++ src/utils/Img.jsx
@@ -1,7 +1,9 @@
 import Galletas from "../assets/img/Galletas.png";
 import GomaHigado from "../assets/img/GomaHigado.png";
+import Robin from "../assets/img/Robin.jpg";
 
 export const Images = {
   Galletas,
   GomaHigado,
+  Robin,
 };
