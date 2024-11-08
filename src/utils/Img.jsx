6cb2a200e56d4d38b07d5c154470836a43331b0a
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-11-08 14:54:26 -0500

    refactor (utils) img gestor

--- src/utils/Img.jsx
+++
@@ -1,9 +0,0 @@
-import Galletas from "../assets/img/Galletas.png";
-import GomaHigado from "../assets/img/GomaHigado.png";
-import Robin from "../assets/img/Robin.jpg";
-
-export const Images = {
-  Galletas,
-  GomaHigado,
-  Robin,
-};
--- src/utils/imageMap.js
+++ src/utils/imageMap.js
@@ -1,3 +1,4 @@
 export const imageMap = {
+  Robin: new URL("../assets/img/Robin.jpg", import.meta.url).href,
   "Galletas.png": new URL("../assets/img/Galletas.png", import.meta.url).href,
 };
